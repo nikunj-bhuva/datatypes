f49e11438630274ce764caa5faf9c82df8f4137e
nikunj-bhuva <nikunjbhuva121@gmail.com>
2024-02-12 19:32:09 +0530

    hello

---
+++ index.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a=10;
+    float b=3.4;
+    b=4.5;
+    char v='a';
+    string abc="ram";
+    bool c=true;
+    cout << "value of a : " << a << endl;
+    cout << "value of b : " << b << endl;
+    cout << "value of v : " << v << endl;
+    cout << "value of c : " << c <<endl;
+    cout << "value of abc : " << abc; 
+
+    return 0;
+}
